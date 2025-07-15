59513bd4b2bbf78a3e21808dfa3b5dbc8803afdd
jayjay-source <jacopofilippi00@gmail.com>
2025-07-15 22:53:34 +0200

    Update application.properties di libro-service

--- prenotazione-service/src/main/java/it/project_work/prenotazione/prenotazione_service/controller/PrenotazioneController.java
+++ prenotazione-service/src/main/java/it/project_work/prenotazione/prenotazione_service/controller/PrenotazioneController.java
@@ -1,13 +1,21 @@
 package it.project_work.prenotazione.prenotazione_service.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import it.project_work.prenotazione.prenotazione_service.dto.PrenotazioneDTO;
 import it.project_work.prenotazione.prenotazione_service.service.PrenotazioneService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -25,7 +33,7 @@ public class PrenotazioneController
 
     @PutMapping
     public PrenotazioneDTO save(@Valid @RequestBody PrenotazioneDTO prenotazioneDTO, @RequestParam String utenteUuid, @RequestParam String libroUuid)
-    { return prenotazioneService.save(prenotazioneDTO); }
+    { return prenotazioneService.save(prenotazioneDTO, utenteUuid, libroUuid); }
 
     @PatchMapping
     public PrenotazioneDTO update(@RequestBody String uuid, @RequestBody PrenotazioneDTO prenotazioneDTO) { return prenotazioneService.update(uuid, prenotazioneDTO); }
