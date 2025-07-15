6ddaf8dfccae389b205aea259a8622041e235094
achiros17 <chironealessio51@gmail.com>
2025-07-15 14:50:18 +0200

    .

--- utenti-service/src/main/java/it/biblioteca/project_work/utente_service/repository/UtenteRepository.java
+++ utenti-service/src/main/java/it/biblioteca/project_work/utente_service/repository/UtenteRepository.java
@@ -4,7 +4,6 @@ import it.biblioteca.project_work.utente_service.entity.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
 public interface UtenteRepository extends JpaRepository<Utente, Long>
 {
     // Metodo per la ricerca di un utente tramite username
