05537c1c6d3a0908c8070a34de4aeed5fac7e21c
achiros17 <chironealessio51@gmail.com>
2025-07-17 00:21:07 +0200

    .

--- prenotazione-service/src/main/java/it/project_work/prenotazione/prenotazione_service/controller/PrenotazioneController.java
+++ prenotazione-service/src/main/java/it/project_work/prenotazione/prenotazione_service/controller/PrenotazioneController.java
@@ -1,26 +1,20 @@
 package it.project_work.prenotazione.prenotazione_service.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import it.project_work.prenotazione.prenotazione_service.dto.PrenotazioneDTO;
 import it.project_work.prenotazione.prenotazione_service.service.PrenotazioneService;
 import jakarta.validation.Valid;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/prenotazioni")
+
 public class PrenotazioneController
 {
     private final PrenotazioneService prenotazioneService;
@@ -33,7 +27,7 @@ public class PrenotazioneController
 
     @PutMapping
     public PrenotazioneDTO save(@Valid @RequestBody PrenotazioneDTO prenotazioneDTO, @RequestParam String utenteUuid, @RequestParam String libroUuid)
-    { return prenotazioneService.save(prenotazioneDTO, utenteUuid, libroUuid); }
+    { return prenotazioneService.save(prenotazioneDTO,utenteUuid,libroUuid); }
 
     @PatchMapping
     public PrenotazioneDTO update(@RequestBody String uuid, @RequestBody PrenotazioneDTO prenotazioneDTO) { return prenotazioneService.update(uuid, prenotazioneDTO); }
