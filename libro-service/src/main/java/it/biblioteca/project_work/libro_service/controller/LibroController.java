3472ed7052098bdbaa8508d2e6ad322ba57cb4f7
jayjay-source <jacopofilippi00@gmail.com>
2025-07-16 12:18:00 +0200

    Aggiornati controller di utente e libro

--- libro-service/src/main/java/it/biblioteca/project_work/libro_service/controller/LibroController.java
+++ libro-service/src/main/java/it/biblioteca/project_work/libro_service/controller/LibroController.java
@@ -1,15 +1,25 @@
 package it.biblioteca.project_work.libro_service.controller;
 
-import it.biblioteca.project_work.libro_service.dto.LibroDTO;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-import it.biblioteca.project_work.libro_service.service.LibroService;
-
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.biblioteca.project_work.libro_service.dto.LibroDTO;
+import it.biblioteca.project_work.libro_service.service.LibroService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/libri")
